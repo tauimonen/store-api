63d783c34bb954cf1e885cb4e268d452857801f4
Toni Uimonen <tauimonen@gmail.com>
2022-10-21 18:27:02 +0300

    Make company and featured queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,27 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors')
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({
+    name: "vase table",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
