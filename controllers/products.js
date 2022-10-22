3c8a947dac5cf7d2351c6c4afb74711b356a9192
Toni Uimonen <tauimonen@gmail.com>
2022-10-22 11:21:32 +0300

    Make product search by name

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'ood table';
   const products = await Product.find({
-    name: "vase table",
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -17,6 +18,10 @@ const getAllProducts = async (req, res) => {
 
   if (company) {
     queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
   console.log(queryObject);
